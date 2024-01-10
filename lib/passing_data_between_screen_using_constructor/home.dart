8e332e3279a05ad861af24dcde396c0ee450ba81
DIVYAKV15 <dhivya.kv1992@gmail.com>
2024-01-10 09:04:55 +0300

    initial commit

--- lib/passing_data_between_screen_using_constructor/home.dart
+++ lib/passing_data_between_screen_using_constructor/home.dart
@@ -1,32 +1,53 @@
 import 'package:flutter/material.dart';
 import 'package:practice_advanced_topic/passing_data_between_screen_using_constructor/to_stateful/stateful_screen.dart';
 import 'package:practice_advanced_topic/passing_data_between_screen_using_constructor/to_stateless/stateless_screen.dart';
-void main()
-{
-  runApp(MaterialApp(home: Home(),));
+
+void main() {
+  runApp(MaterialApp(
+    home: Home(),
+  ));
 }
+
 class Home extends StatelessWidget {
- Home({super.key});
-  String name="LUMINAR";
-  String designation="Intern";
-  var experience=6;
+  Home({super.key});
+  String name = "LUMINAR";
+  String designation = "Intern";
+  var experience = 6;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(title: const Text("Data Passing")),
-    body: Center(
-      child:
-      Column(
-        children: [
-         ElevatedButton(onPressed: (){
-           Navigator.push(context, MaterialPageRoute(builder: (context)=>StateLessScreen(name:name,designation:designation,experience:experience)));
-         }, child: const Text("TO Stateless Widget"),),
-          ElevatedButton(onPressed: (){
-      Navigator.push(context, MaterialPageRoute(builder: (context)=>StateFulscreen(name:name,designation:designation,experience:experience)));
-    },
-           child: const Text("TO Stateful Widget"),),
-        ],
+    return Scaffold(
+      appBar: AppBar(title: const Text("Data Passing")),
+      body: Center(
+        child: Column(
+          children: [
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StateLessScreen(
+                            name: name,
+                            designation: designation,
+                            experience: experience)));
+              },
+              child: const Text("TO Stateless Widget"),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => StateFulscreen(
+                            name: name,
+                            designation: designation,
+                            experience: experience)));
+              },
+              child: const Text("TO Stateful Widget"),
+            ),
+          ],
+        ),
       ),
-    ),);
+    );
   }
 }
